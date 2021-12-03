a37f52c80467732737497cc93f20982bf67a294a
DuongTT <trantungduong99@gmail.com>
2021-12-03 23:35:33 +0700

    test metadata

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const moment = require('moment')
+const { HOST } = require('./src/constants')
+const db = require('./src/database')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+  .set('port', PORT)
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+
+// Static public files
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res) {
+  res.send('Get ready for OpenSea!');
+})
+
+app.get('/api/token/:token_id', function(req, res) {
+  const tokenId = parseInt(req.params.token_id).toString()
+  const tooZoo = db[tokenId]
+  res.send(tooZoo)
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+})
---
+++ src/database.js
@@ -0,0 +1,107 @@
+module.exports = {
+  "1": {
+    "attributes": [
+        {
+            "trait_type": "base",
+            "value": "fire"
+        },
+        {
+            "trait_type": "head",
+            "value": "fire"
+        },
+        {
+            "trait_type": "legs",
+            "value": "fire"
+        },
+        {
+            "trait_type": "tail",
+            "value": "fire"
+        },
+        {
+            "trait_type": "back",
+            "value": "fire"
+        },
+        {
+            "trait_type": "gender",
+            "value": "male"
+        },
+        {
+            "trait_type": "personality",
+            "value": "sleepy"
+        }
+    ],
+    "description": "Pure Fire",
+    "image": "https://topchuan.com/wp-content/uploads/2019/09/Kirin-Kirin.jpg",
+    "name": "Tran"
+  },
+  "2": {
+      "attributes": [
+          {
+              "trait_type": "base",
+              "value": "water"
+          },
+          {
+              "trait_type": "head",
+              "value": "water"
+          },
+          {
+              "trait_type": "legs",
+              "value": "water"
+          },
+          {
+              "trait_type": "tail",
+              "value": "water"
+          },
+          {
+              "trait_type": "back",
+              "value": "water"
+          },
+          {
+              "trait_type": "gender",
+              "value": "male"
+          },
+          {
+              "trait_type": "personality",
+              "value": "Hihi"
+          }
+      ],
+      "description": "Pure Water",
+      "image": "http://pppre.s3.amazonaws.com/371d4940d912fc92/ea1f1665234f4ae0ae9cfe8a89cdb3f7.jpg",
+      "name": "Tung"
+  },
+  "3": {
+      "attributes": [
+          {
+              "trait_type": "base",
+              "value": "wind"
+          },
+          {
+              "trait_type": "head",
+              "value": "wind"
+          },
+          {
+              "trait_type": "legs",
+              "value": "wind"
+          },
+          {
+              "trait_type": "tail",
+              "value": "wind"
+          },
+          {
+              "trait_type": "back",
+              "value": "wind"
+          },
+          {
+              "trait_type": "gender",
+              "value": "male"
+          },
+          {
+              "trait_type": "personality",
+              "value": "Fassa"
+          }
+      ],
+      "description": "Pure Winter",
+      "image": "https://cdna.artstation.com/p/assets/images/images/007/080/778/large/angelica-kc-sparks-claudio-wind-snow-leopard-finished.jpg",
+      "name": "Duong"
+  }
+}
